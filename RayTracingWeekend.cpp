09c06d3fb9a048db4066affc4d0b656cd531ec64
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-11-29 12:54:20 +0100

    Simple sphere intersection

--- RayTracingWeekend.cpp
+++ RayTracingWeekend.cpp
@@ -5,10 +5,30 @@
 #include <iostream>
 
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 // Create sky gradient background
 color ray_color(const ray& r) {
+
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
+    // Extract direction vector of ray
     vec3 unit_direction = unit_vector(r.direction());
+
+    // Extract y and scale from 0.0 to 1.0
+    // a = 0 - ray downward
+    // a = 1 - ray upward
     auto a = 0.5 * (unit_direction.y() + 1.0);
+
+    // Blend white and blue based on a's direction
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
@@ -30,6 +50,7 @@ int main() {
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
 
+    // Calculate size of a single pixel's width and height in the viewport space.
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
