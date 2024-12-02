2f089f79f60469fa45b33045823f6f11b39cef3a
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-12-02 20:21:54 +0100

    Changes to sphere and hittable

--- hittable.h
+++ hittable.h
@@ -9,6 +9,18 @@ class hit_record
 		point3 p;
 		vec3 normal;
 		double t;
+		bool front_face;
+
+		void set_face_normal(const ray& r, const vec3& outward_normal) {
+			// Sets the hit record normal vector.
+			// NOTE: the parameter `outward_normal` is assumed to have unit length.
+			
+			// Determine if ray is hitting front face or back face
+			// Ensure normal vector always points against the ray's direction
+			front_face = dot(r.direction(), outward_normal) < 0;
+			normal = front_face ? outward_normal : -outward_normal;
+
+		}
 };
 
 class hittable {
